13b740b3c4eb9fa3c994205bc7673485f3fe01cc
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-06-13 16:28:46 +0200

    added booleano in $todoList in server.php, added cycle v for in index.html and print element.fare and element.cosa, and added function in main.js that added class in html with bindig class when click an element

--- client-side/main.js
+++ client-side/main.js
@@ -3,19 +3,27 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            welcomeMessage: "Ciao vue funziona",
+            listMessage: "La lista di cose da fare",
             print: []
         }
     },
     methods: {
+        //FUNZIONE CHE AL CLICK MI PASSA IL BOOLEANO DA TRUE A FALSE COSI CHE NELLA FUNZIONA DOPO GLI POSSO AGGIUNGERE LA CLASSE
+        elementDone(index) {
+            this.print[index].booleano = false
+        },
 
+        //FUNZIONE CHE QUANDO CLICCO SU LI IN INDEX MI PASSA IL BOOLEAN A FALSO E MI AGGIUNGE UNA RIGA SOPRA LA COSA CLICCATA
+        lineTrough(index) {
+            return (this.print[index].booleano == false) ? "text-decoration-line-through" : ""
+        }
     },
     mounted() {
         console.log("Funziona tutto");
 
         axios.get("../server.php").then(result => {
             console.log(result.data)
-            this.print = result
+            this.print = result.data
             console.log(this.print)
         })
     }
--- server.php
+++ server.php
@@ -4,21 +4,25 @@ $todoList = [
     [
         'fare' => 'Cucinare',
         'cosa' => 'Pasta e patate',
+        'booleano' => true
     ],
 
     [
         'fare' => 'Pulire',
         'cosa' => 'Lavare pavimento',
+        'booleano' => true
     ],
 
     [
         'fare' => 'Fare la spesa',
         'cosa' => 'Comprare Acqua',
+        'booleano' => true
     ],
 
     [
         'fare' => 'Giocare',
         'cosa' => 'Giocare a cod',
+        'booleano' => true
     ],
 ];
 
